c0c32959fed01fed84b189fff46be9193b614132
손재민 <jasson31@naver.com>
2018-11-16 14:01:26 +0900

    필요한 레이아웃 추가

--- app/src/main/java/com/jasson31/menuselector/Main_Select.java
+++ app/src/main/java/com/jasson31/menuselector/Main_Select.java
@@ -1,60 +1,70 @@
 package com.jasson31.menuselector;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main_Select extends AppCompatActivity{
+    public static final ArrayList<Restaurant> testRestaurant = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_select);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        testRestaurant.add(new Restaurant("A", 2));
+        testRestaurant.add(new Restaurant("B", 3));
+        testRestaurant.add(new Restaurant("C", 1));
+        Button randomizer = (Button) findViewById(R.id.button_randomize);
+        randomizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Randomizer();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_test, menu);
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        TextView result = (TextView) findViewById(R.id.text_test);
         switch(item.getItemId()){
-            case R.id.action_next_page:
+            case R.id.action_config:
                 Toast.makeText(this, "to next page", Toast.LENGTH_LONG).show();
-                result.setText("다음 페이지로");
                 Intent intent = new Intent(Main_Select.this, Sub_Setting.class);
                 startActivity(intent);
-                return true;
-            case R.id.action_setting:
-                Toast.makeText(this, "setting", Toast.LENGTH_LONG).show();
-                result.setText("설정");
-                return true;
-            case android.R.id.home:
-                result.setText("뒤로가기");
-                Toast.makeText(this, "홈 버튼 터치", Toast.LENGTH_LONG).show();
-                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void Randomizer(){
+        TextView result = (TextView) findViewById(R.id.text_result);
+        int randomIndex = new Random().nextInt(3);
+        result.setText(testRestaurant.get(randomIndex).getName());
+        for(int i = 0; i < testRestaurant.size(); i++){
+            if(i != randomIndex)
+                continue;
+            testRestaurant.get(randomIndex).setPreference(testRestaurant.get(randomIndex).getPreference() + 1);
+        }
+        //Toast.makeText(this, testRestaurant.get(randomIndex).getPreference(), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/jasson31/menuselector/Sub_Setting.java
+++ app/src/main/java/com/jasson31/menuselector/Sub_Setting.java
@@ -1,86 +1,98 @@
 package com.jasson31.menuselector;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Sub_Setting extends AppCompatActivity {
-    static final ArrayList<Restaurant> testRestaurant = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_setting);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        testRestaurant.add(new Restaurant("A", 2));
-        testRestaurant.add(new Restaurant("B", 3));
-        testRestaurant.add(new Restaurant("C", 1));
+        //CreateListView();
+        /*Main_Select.testRestaurant.add(new Restaurant("A", 2));
+        Main_Select.testRestaurant.add(new Restaurant("B", 3));
+        Main_Select.testRestaurant.add(new Restaurant("C", 1));
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
-        for(int i = 0; i < testRestaurant.size(); i++){
-            adapter.add((i + 1) + ". " + testRestaurant.get(i).getName() + " " + testRestaurant.get(i).getPreference());
+        for(int i = 0; i < Main_Select.testRestaurant.size(); i++){
+            adapter.add((i + 1) + ". " + Main_Select.testRestaurant.get(i).getName() + " " + Main_Select.testRestaurant.get(i).getPreference());
         }
-        ListView listView = (ListView) findViewById(R.id.ListViewTest);
+        final ListView listView = (ListView) findViewById(R.id.ListViewTest);
         listView.setAdapter(adapter);
-        //final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items);
-
-        /*ListView listView;
-        ListViewAdapter adapter;
-        adapter = new ListViewAdapter();
-        listView = (ListView) findViewById(R.id.ListViewTest);
-        listView.setAdapter(adapter);
-        adapter.addItem("Box", "Account Box Black");
-        adapter.addItem("Circle", "Account Circle Black");
-        adapter.addItem("Ind", "Assignment Ind Black");
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TestItem item = (TestItem) parent.getItemAtPosition(position);
-                String titleStr = item.getTitle();
-                String descStr = item.getDesc();
-                Toast.makeText(Sub_Setting.this, titleStr + "\n" + descStr, Toast.LENGTH_SHORT).show();
+                Toast.makeText(Sub_Setting.this, (String)parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
             }
         });*/
-
+        CreateListView();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_test, menu);
+        inflater.inflate(R.menu.menu_sub, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
+            case R.id.action_add:
+                AlertDialog.Builder ad = new AlertDialog.Builder(Sub_Setting.this);
+                ad.setTitle("Title");
+                ad.setMessage("Message");
+                final EditText et = new EditText(Sub_Setting.this);
+                ad.setView(et);
+                ad.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Sub_Setting.this, et.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                ad.show();
+                return true;
             case android.R.id.home:
                 finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    public void CreateListView(){
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        for(int i = 0; i < Main_Select.testRestaurant.size(); i++){
+            adapter.add((i + 1) + ". " + Main_Select.testRestaurant.get(i).getName() + " " + Main_Select.testRestaurant.get(i).getPreference());
+        }
+        final ListView listView = (ListView) findViewById(R.id.ListViewTest);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Sub_Setting.this, (String)parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
